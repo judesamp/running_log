89e9b761e7f5b98f0d0bf40d9fd15d6d9b236968
Jeremy Samples <judesamp@gmail.com>
2014-05-21 15:34:41 -0400

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to user_runs_path(user), notice: "Thanks for signing up. You've also been logged in."
     else
       redirect_to users_path, notice: "Something went wrong:) Please try logging in again."
