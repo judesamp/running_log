d3f7caa7d947674cac17609616864bbad362940f
Jeremy Samples <judesamp@gmail.com>
2014-05-21 14:56:46 -0400

    working login fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :pretty_date, :pretty_pace, :current_user
-  # before_filter :ensure_logged_in
+  before_filter :ensure_logged_in
 
   private
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def ensure_logged_in
