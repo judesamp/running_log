1466d913c8d9807ac9ad913559f44dd954e6d41d
Jeremy Samples <judesamp@gmail.com>
2014-05-27 08:54:22 -0400

    fiddling with routing and errors

--- app/controllers/error_pages_controller.rb
+++ app/controllers/error_pages_controller.rb
@@ -1,7 +1,7 @@
 class ErrorPagesController < ApplicationController
 
   def handle_404
-    redirect_to root_path, notice: "Sorry! That url doesn't exist or you don't have access to it."
+    redirect_to root_path, notice: "Sorry! Either that url does not exist or you do not have access to it."
   end
 
 end
